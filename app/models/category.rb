660e76c6e41ed2f21bb0243b20fb0184187149e9
John Magee <jemagee@cox.net>
2016-02-25 18:24:11 -0800

    Smallish tweak to category model testing custom error messages by model spec testing

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :articles
 
-  validates :name, presence: true, uniqueness: { case_sensitive: false}
+  validates :name, presence: true, uniqueness: { case_sensitive: false, message: "This category already exists"}
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   it { should validate_presence_of(:name)}
-  it { should validate_uniqueness_of(:name).case_insensitive}
+  it { should validate_uniqueness_of(:name).case_insensitive.with_message("This category already exists")}
   it { should have_many(:articles)}
 end
