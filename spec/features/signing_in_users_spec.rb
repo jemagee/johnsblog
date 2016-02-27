da585662c31df4a6f3438d700fffdc67603b2d23
John Magee <jemagee@cox.net>
2016-02-27 13:36:29 -0800

    Building upon Devise functionality, testing signing in and altering layout accordingly

---
+++ spec/features/signing_in_users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Users can sign in" do
+  let!(:user) {FactoryGirl.create(:user)}
+
+  scenario "With Valid Credentials" do
+    visit "/"
+    click_link "Sign In"
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Log in"
+
+    expect(page).to have_content("Signed in successfully")
+    expect(page).to have_content(user.email)
+    expect(page).to have_link("Sign Out")
+  end
+end
