2e93eb3b4f8d69b20fdd2dfbf764da8f422a0abe
John Magee <jemagee@cox.net>
2016-03-08 20:46:24 -0800

    Adding Redcarpet and changing the article show view so the body of the article can be written in (and processed as) markup

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'sass-rails', '~> 5.0'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'font-awesome-rails', '~> 4.5'
 gem 'haml'
+gem 'redcarpet'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def admins_only(&block)
     block.call if current_user.try(:admin?)
   end
+
+  def redcarpet_process(input)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    markdown.render(input).html_safe
+  end
 end
