daf6f330323b525e5377460e27326847b0f7a66a
John Magee <jemagee@cox.net>
2016-03-23 19:34:13 -0700

    Testing some layout changes on the index page regarding publication display and comment count, requiring date helpers into rspec

--- spec/features/non_admin_index_spec.rb
+++ spec/features/non_admin_index_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature "Testing the Public article index" do
 	let!(:published) {FactoryGirl.create(:article, published_on: Date.today, status: "published")}
 	let!(:oldarticle) {FactoryGirl.create(:article, published_on: Date.today.days_ago(60), status: "published")}
 	let!(:newest) {FactoryGirl.create(:article, status: "published", published_on: Date.today.months_since(3))}
+	let!(:comment) {FactoryGirl.create(:comment, article: published)}
 
 	context "As an anonymous user" do
 
@@ -17,22 +18,28 @@ RSpec.feature "Testing the Public article index" do
 			expect(page).to_not have_content(draft.title)
 		end
 
-		scenario "should have published and archived articles" do
+		scenario "should have published articles" do
 			expect(page).to have_content(published.title)
 			expect(page).to have_content(oldarticle.title)
 			expect(page).to have_content(newest.title)
 		end
 
-		scenario "Published articles should be displayed newest to oldest" do
+
+		scenario "Published articles should be displayed newest to oldest, with comment count" do
 			within("#articles") do
 				within("div:nth-child(1)") do
 					expect(page).to have_content(newest.title)
+					expect(page).to have_content("Published #{time_ago_in_words(newest.published_on)}")
+					expect(page).to have_content("Comments: 0")
 				end
 				within("div:nth-child(2)") do
 					expect(page).to have_content(published.title)
+					expect(page).to have_content("Published #{time_ago_in_words(published.published_on)}")
+					expect(page).to have_content("Comments: 1")
 				end
 				within("div:nth-child(3)") do
 					expect(page).to have_content(oldarticle.title)
+					expect(page).to have_content("Published #{time_ago_in_words(oldarticle.published_on)}")
 				end
 			end
 		end
