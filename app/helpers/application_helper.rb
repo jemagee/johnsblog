a89a4baa9d295ad161779c2c407f8d794e7a0d33
John Magee <jemagee@cox.net>
2016-04-07 19:37:41 -0700

    Cleaning up some styling aspects and improving the Redcarpet for codeblocks and link attributes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,8 @@ module ApplicationHelper
   end
 
   def redcarpet_process(input)
-    renderer = Redcarpet::Render::HTML.new(filter_html: true)
-    markdown = Redcarpet::Markdown.new(renderer)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, link_attributes: {target: "_blank"})
+    markdown = Redcarpet::Markdown.new(renderer, {fenced_code_blocks: true, disable_indented_code_blocks: true})
     markdown.render(input).html_safe
   end
 end
