42468d392c9ada48f013a3b6bdb31c3ba24ff098
John Magee <jemagee@cox.net>
2016-04-25 20:07:48 -0700

    Adding and testing the sanitizer to comments in the steps to switch over to kramdown

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ActiveRecord::Base
+
+  before_save :clean_comment
   belongs_to :article
   validates :body, presence: true
+
+  def clean_comment
+    sanitizer = Rails::Html::FullSanitizer.new
+    self.body = sanitizer.sanitize(self.body)
+  end
 end
--- spec/features/creating_comment_spec.rb
+++ spec/features/creating_comment_spec.rb
@@ -25,4 +25,26 @@ RSpec.feature "Creating a comment associated with a blog" do
     expect(page).to have_content("Your comment has not been created")
     expect(page).to_not have_content("This is a silly comment without a title")
   end
+
+  scenario "A Comment doesn't allow html to be processed" do
+    within("#new_comment_form") do
+      fill_in "comment_body", with: "This is a silly little <b>bold comment</b>"
+    end
+
+    click_button "Add Comment"
+    expect(page).to have_content("Your comment has been created")
+    expect(page).to_not have_content("<b>bold comment</b>")
+    expect(page).to have_content("little bold comment")
+  end
+
+  scenario "A comment won't process a link either" do
+    within ("#new_comment_form") do
+      fill_in "comment_body", with: '<a href="http://espn.go.com">Espn Web Site</a>'
+    end
+
+    click_button "Add Comment"
+    expect(page).to have_content("Your comment has been created")
+    expect(page).to have_content("Espn Web Site")
+    expect(page).to_not have_link('espn.go.com')
+  end
 end
