a1e7cc61c05844e6e6e956db20d03ef894d86f02
John Magee <jemagee@cox.net>
2017-01-17 20:02:03 -0800

    Creating the basic tag model and model testing the simple validations on it

---
+++ app/models/tag.rb
@@ -0,0 +1,4 @@
+class Tag < ActiveRecord::Base
+
+	validates :name, presence: true, uniqueness: {case_sensitive: false}
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105032620) do
+ActiveRecord::Schema.define(version: 20170118035840) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 20170105032620) do
   create_table "pictures", force: :cascade do |t|
     t.string   "name"
     t.string   "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
